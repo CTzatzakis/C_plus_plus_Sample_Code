da0f7e0ce7cf05ec0428dcb760d680f92bebb780
CTzatzakis <>
2018-07-15 12:38:31 +0300

    First introductory lessons
    Adding learning material

---
+++ 2_Input/main_input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    float radius,perim,area;
+    const float PI = 3.14159;
+
+    std::cout <<"Give circle R:";
+    std::cin >> radius;
+
+    perim = 2* PI*radius;
+    area = PI * radius * radius;
+    std::cout <<std::setw(12)<<"Perimeter:"<<std::setw(5)<<std::setprecision(9)<<perim<<std::endl;
+    std::cout <<std::setw(12)<<"Area:"<<std::setw(5)<<std::setprecision(9)<<area<<std::endl;
+
+    return 0;
+}
--- 6_For_Loop/main.cpp
+++ 6_For_Loop/main.cpp
@@ -2,7 +2,12 @@
 
 int main() {
     int i=0,j=0,N=10,M=10;
-
+    std::cout << "Count to Ten" << std::endl;
+    for (i = 0; i < N; i++)
+    {
+        std::cout << i+1 << std::endl;
+    }
+    std::cout << "Stars DESC" << std::endl;
     for(i=0; i<N; i++)
     {
         for(j=i; j<M; j++)
@@ -11,7 +16,7 @@ int main() {
         } std::cout << std::endl;
     }
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Stars ASC" << std::endl;
     M=9;
     for(i=0; i<N; i++)
     {
