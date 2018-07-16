eb6dd0213d808910116b5d1a9b5654202d9ed3ec
Chris Tzatzakis <33452504+CTzatzakis@users.noreply.github.com>
2018-07-16 17:52:11 +0300

    First introductory lessons

--- 1_Intro/1_6_For_Loop/main.cpp
+++ 1_Intro/1_6_For_Loop/main.cpp
@@ -2,11 +2,13 @@
  
 int main() {
     int i=0,j=0,N=10,M=10;
+ 
     std::cout << "Count to Ten" << std::endl;
     for (i = 0; i < N; i++)
     {
         std::cout << i+1 << std::endl;
     }
+ 
     std::cout << "Stars DESC" << std::endl;
     for(i=0; i<N; i++)
     {
